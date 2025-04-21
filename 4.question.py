c48a02737fe54bd046a8eaf404a09bccd23bdf0d
Elide <zavalavinagreelide@gmail.com>
2025-04-20 20:21:16 -0600

    question 4

--- 4.question.py
+++ 4.question.py
@@ -1,1 +1,7 @@
 #  Given a string and an integer number n, remove characters from a string starting from zero up to n and return a new string
+
+def remove_chars(str, num):
+    return str[num:] # Con el num: obtenemos el resto de los valores
+
+
+print(remove_chars("learning", 4))
