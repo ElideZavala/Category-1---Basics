ceab5b90e3b48a5ae3412e8a8fcc1f106da6cf89
Elide <zavalavinagreelide@gmail.com>
2025-04-20 19:17:24 -0600

    question 3

--- 3.question.py
+++ 3.question.py
@@ -1,1 +1,7 @@
-# In a string, display the characters at the even indices
+def print_even_index(str):
+
+    for char in range(0, len(str), 2):
+        print("Index[",char,"]", str[char] )
+
+string = "Elide Zavala"
+print_even_index(string)
