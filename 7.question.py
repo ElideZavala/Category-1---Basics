906dc714b60479d7a410678d3c037c03b9d54dc9
Elide <zavalavinagreelide@gmail.com>
2025-04-27 11:10:06 -0600

    question 7

--- 7.question.py
+++ 7.question.py
@@ -1,3 +1,13 @@
 # Return the count of how many times the sub-string “Katie” appears in the given string
 
 str = "Katie is good developer. Katie is a writer"
+
+# Primera forma
+def some_times(string):
+    return string.count("Katie") # 2
+
+print(some_times(str))
+
+# Segunda forma
+str_count = str.count("Katie")
+print(str_count) # 2
