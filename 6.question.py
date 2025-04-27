90ca357b33ea249675d64372537c67fdf1eaeb50
Elide <zavalavinagreelide@gmail.com>
2025-04-27 10:55:48 -0600

    question 6

--- 6.question.py
+++ 6.question.py
@@ -1,3 +1,12 @@
 # Given a list of numbers, Iterate it and print only those numbers which are divisible of 5
 
 list = [10, 20, 33, 46, 55, 77, 105, 204, 335]
+
+def divid_of_five(numbers):
+    for num in numbers:
+        if (num % 5 == 0):
+            print(f"{num}, {num} % 5 = {num%5}")
+
+
+divid_of_five(list)
+
