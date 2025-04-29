b6a139a39ae4e770f8b31f383b467a64682052c3
Elide <zavalavinagreelide@gmail.com>
2025-04-28 21:06:48 -0600

    8 question

--- 8.question.py
+++ 8.question.py
@@ -7,3 +7,19 @@
 4 4 4 4 
 5 5 5 5 5
 """
+
+def triangulo(n):
+    for i in range(1, n + 1):
+        value = [i] * i
+        print(" ".join(map(str, value)))
+
+        # (map(str, value)) = cada value lo va a combertir en un string, siendo recorrido gracias a un map.
+        # con el join, lo unimos, separandolo solo por un espacio.
+
+triangulo(5)
+
+for num in range(6):
+    for i in range(num):
+        print (num, end=" ")
+    
+    print("\n")
