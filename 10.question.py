1fbb506d671f530da0ba16268a5a364f2b0da692
Elide <zavalavinagreelide@gmail.com>
2025-05-04 13:22:42 -0600

    question 10

--- 10.question.py
+++ 10.question.py
@@ -2,3 +2,24 @@
 
 list1 = [10, 20, 23, 11, 17, 44, 55]
 list2 = [13, 43, 24, 36, 12, 33, 66]
+
+def create_list_odd(lists):
+
+    revered_list = []
+    for number in lists:
+        if  number % 2 == 0 :
+            revered_list.append(number)
+
+    return revered_list
+    
+def create_list_pars(lists):
+    pars_numbers = []
+
+    for number in lists:
+        if  number % 2 != 0 :
+            pars_numbers.append(number)
+
+    return pars_numbers
+
+print( create_list_odd(list1))
+print( create_list_pars(list2))
