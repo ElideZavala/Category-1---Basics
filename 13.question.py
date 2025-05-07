1bfe752d52a6d48bb0decd5d7fd045268c0e08e7
Elide <zavalavinagreelide@gmail.com>
2025-05-06 21:47:59 -0600

    question 13

--- 13.question.py
+++ 13.question.py
@@ -1,1 +1,14 @@
 #  Print multiplication table from 1 to 10
+
+rango = 11
+
+for i in range(1, rango):
+    print(f"## Tabla del {i} ##")
+
+    for j in range(1, rango):
+        print((f"{j} * {i} = {i * j}" ))
+        if (j == 10):
+            print("\n")
+
+
+# EN EL 1 VAN HABER 10, POR QUE ESTA DENTRO DEL BUCLE
