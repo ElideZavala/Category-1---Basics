bb56c802c2e32c0c53d74ea0f38f81476fe6f0a2
Elide <zavalavinagreelide@gmail.com>
2025-05-10 21:12:01 -0600

    question 15

--- 15.question.py
+++ 15.question.py
@@ -1,1 +1,16 @@
 # Write a function called exponent(base, exp) that returns an int value of base raised to the power of exp.
+# def exponent(base, exp):
+#     return base ** exp
+
+# print(exponent(5 , 4))
+
+def exponent(base, exp):
+    num = exp
+    result = 1
+
+    while num > 0:
+        result = result * base
+        print(result) # 5, 25, 125, 625, None
+        num = num - 1
+
+print(exponent(5 , 4)) # 625
